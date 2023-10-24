af2ce2dcbec9684dbc55b11f3873e5b68127db6b
stefaniaroman15 <148216286+stefaniaroman15@users.noreply.github.com>
2023-10-24 22:33:02 +0300

    button added

--- Twixt/Twixt/main.cpp
+++ Twixt/Twixt/main.cpp
@@ -3,8 +3,14 @@
 //SFML sample code - try to run
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Twixt Game");
+    // Open the window
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Twixt Game");
     window.setFramerateLimit(60);
+
+    // Declare the "Start" button
+    sf::RectangleShape startButton(sf::Vector2f(200, 50));
+    startButton.setFillColor(sf::Color::Cyan);
+    startButton.setPosition(860, 490);
 
     // Main game loop
     while (window.isOpen()) {
@@ -18,7 +24,8 @@ int main() {
         // Clear the window
         window.clear(sf::Color::White);
 
-        // Draw your start menu options here
+        // Add button
+        window.draw(startButton);
 
         // Display what you have drawn
         window.display();

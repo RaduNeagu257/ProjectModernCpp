e9ffd69391c7b6e14555d55b35a385f47abcaf86
stefaniaroman15 <148216286+stefaniaroman15@users.noreply.github.com>
2023-10-25 00:55:18 +0300

    Title added

--- Twixt/Twixt/main.cpp
+++ Twixt/Twixt/main.cpp
@@ -15,12 +15,21 @@ int main() {
     // Declare the text for the "Start" button
     sf::Font font;
     font.loadFromFile("ARIAL.TTF");
+    sf::Font titleFont;
+    titleFont.loadFromFile("TitleText.ttf");
     sf::Text startButtonText("Play!", font, 24);
     startButtonText.setFillColor(sf::Color::Blue);
     // Center the text on the button
     startButtonText.setPosition(startButton.getPosition().x + (startButton.getSize().x - startButtonText.getGlobalBounds().width) / 2,
         startButton.getPosition().y + (startButton.getSize().y - startButtonText.getGlobalBounds().height) / 2);
- 
+    // Set the title of the window
+    sf::Text titleText("Twixt Game", titleFont, 36); // You can adjust the font size
+    titleText.setFillColor(sf::Color::Black); // Set the color
+    titleText.setStyle(sf::Text::Bold); // Make it bold
+    // Center the title above the button
+    titleText.setPosition(860 + (startButton.getSize().x - titleText.getGlobalBounds().width) / 2,
+        440);
+
     // Main game loop
     while (window.isOpen()) {
         sf::Event event;
@@ -34,9 +43,10 @@ int main() {
         window.clear(sf::Color::White);
 
         // Add button
+        window.draw(titleText);
         window.draw(startButton);
         window.draw(startButtonText);
-
+     
         // Display what you have drawn
         window.display();
     }

84abdb8a6415059a9599bcb5038e4fa93a7cd8e9
oancea-andrei <andrei.oancea@student.unitbv.ro>
2023-10-25 13:53:00 +0300

    #7 Instructions window added

--- Twixt/Twixt/main.cpp
+++ Twixt/Twixt/main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include<iostream>
 
 //SFML sample code - try to run
 
@@ -42,7 +44,7 @@ int main() {
     instructionsFont.loadFromFile("ARIAL.TTF");
     sf::Font instructionsTitleFont;
     instructionsTitleFont.loadFromFile("TitleText.ttf");
-    sf::Text instructionsButtonText("Instructions!", font, 24);
+    sf::Text instructionsButtonText("Instructions", font, 24);
     instructionsButtonText.setFillColor(sf::Color::Black);
 
 
@@ -58,6 +60,9 @@ int main() {
     titleText.setPosition(860 + (startButton.getSize().x - titleText.getGlobalBounds().width) / 2,
         440);
 
+    bool instructionsWindowOpen = false;
+    sf::RenderWindow instructionsWindow;
+
   
     // Main game loop
     while (window.isOpen()) {
@@ -66,8 +71,34 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            // Check if the "Instructions" button is clicked
+            if (event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                if (instructionButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePosition))) {
+                    instructionsWindowOpen = true;
+                    std::cout << "Instructions button clicked!" << std::endl; // Debug message
+                }
+            }
+        
         }
+        if (instructionsWindowOpen) {
+            if (!instructionsWindow.isOpen()) {
+                instructionsWindow.create(sf::VideoMode(800, 600), "Instructions", sf::Style::Close);
+                instructionsWindow.setFramerateLimit(60);
+            }
 
+            sf::Event instructionsEvent;
+            while (instructionsWindow.pollEvent(instructionsEvent)) {
+                if (instructionsEvent.type == sf::Event::Closed) {
+                    instructionsWindow.close();
+                    instructionsWindowOpen = false;
+                }
+            }
+
+            instructionsWindow.clear(sf::Color::White);
+           
+            instructionsWindow.display();
+        }
         // Clear the window
         window.clear(sf::Color::White);
 
@@ -82,7 +113,8 @@ int main() {
         window.display();
     }
 
-
-
+   
     return 0;
 }
+
+    

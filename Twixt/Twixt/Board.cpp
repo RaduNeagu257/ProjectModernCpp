ee766361ecb9393e764dc817b517f633dd590f78
RaduNeagu257 <148216198+RaduNeagu257@users.noreply.github.com>
2023-10-29 17:04:22 +0200

    Changed the array to show dots instead of squares, added background color for the board and vertical/horizontal lines which show the borders of the game

--- Twixt/Twixt/Board.cpp
+++ Twixt/Twixt/Board.cpp
@@ -8,13 +8,34 @@ Board::Board()
 	{
 		for (int j = 0; j < m_size; ++j)
 		{
-			sf::RectangleShape tile(sf::Vector2f(m_tileSize, m_tileSize));
+			// Create the 2D array for the board
+			sf::CircleShape tile(DOT_RADIUS);
 			tile.setPosition(i * m_tileSize, j * m_tileSize);
 			tile.setOutlineColor(sf::Color::Black);
 			tile.setOutlineThickness(1);
 			m_tiles.push_back(tile);
 		}
 	}
+
+	// Create the red horizontal lines
+	float lineWidth = (m_tileSize + 1) * m_size;
+	m_redHorizontalLine1.setSize({ lineWidth, LINE_THICKNESS });
+	m_redHorizontalLine1.setFillColor(sf::Color::Red);
+	m_redHorizontalLine1.setPosition(0, m_tileSize);
+
+	m_redHorizontalLine2.setSize({ lineWidth, LINE_THICKNESS });
+	m_redHorizontalLine2.setFillColor(sf::Color::Red);
+	m_redHorizontalLine2.setPosition(0, (m_size - 1) * m_tileSize - 2);
+
+	// Create the black vertical lines
+	float lineHeight = (m_tileSize + 1) * m_size;
+	m_blackVerticalLine1.setSize({ LINE_THICKNESS, lineHeight });
+	m_blackVerticalLine1.setFillColor(sf::Color::Black);
+	m_blackVerticalLine1.setPosition(m_tileSize, 0);
+
+	m_blackVerticalLine2.setSize({ LINE_THICKNESS, lineHeight });
+	m_blackVerticalLine2.setFillColor(sf::Color::Black);
+	m_blackVerticalLine2.setPosition((m_size - 1) * m_tileSize - 2, 0);
 }
 void Board::Draw(sf::RenderWindow& window)
 {
@@ -25,11 +46,36 @@ void Board::Draw(sf::RenderWindow& window)
 	// Calculate the distance between the corner of the window and the window to be centered
 	float offsetX = (window.getSize().x - totalBoardWidth) / 2;
 	float offsetY = (window.getSize().y - totalBoardHeight) / 2;
+	
+	// Create the background rectangle
+	sf::RectangleShape backgroundRectangle(sf::Vector2f(totalBoardWidth + m_size, totalBoardHeight + m_size));
 
+	// Set its position to the top-left corner of the array
+	backgroundRectangle.setPosition(offsetX - m_tileSize * 0.8, offsetY - m_tileSize * 0.8);
+
+	// Set its color to rgb(247, 255, 208)
+	backgroundRectangle.setFillColor(sf::Color(247, 255, 208));
+
+	// Draw the background rectangle
+	window.draw(backgroundRectangle); // draw the colored rectangle
+
+	// Centering the array using the previously calculated offsets
 	for (auto& tile : m_tiles)
 	{
-		sf::RectangleShape centeredTile = tile;
-		centeredTile.setPosition(tile.getPosition().x + offsetX, tile.getPosition().y + offsetY); // Centering each tile of the array using the previously calculated offset
+		sf::CircleShape centeredTile = tile;
+		centeredTile.setPosition(tile.getPosition().x + offsetX, tile.getPosition().y + offsetY);
 		window.draw(centeredTile);
 	}
+
+	// Position of the red and black lines showing the borders of each player
+	m_redHorizontalLine1.setPosition(offsetX - m_tileSize * 0.8, offsetY + DOT_RADIUS * 3.5);
+	m_redHorizontalLine2.setPosition(offsetX - m_tileSize * 0.8, offsetY + (m_size - 1) * m_tileSize - DOT_RADIUS * 2);
+	m_blackVerticalLine1.setPosition(offsetX + DOT_RADIUS * 3.5, offsetY - m_tileSize * 0.8);
+	m_blackVerticalLine2.setPosition(offsetX + (m_size - 1) * m_tileSize - DOT_RADIUS * 2, offsetY - m_tileSize * 0.8);
+
+	// Draw the red and black lines
+	window.draw(m_redHorizontalLine1);
+	window.draw(m_redHorizontalLine2);
+	window.draw(m_blackVerticalLine1);
+	window.draw(m_blackVerticalLine2);
 }
--- Twixt/Twixt/Board.h
+++ Twixt/Twixt/Board.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+#define DOT_RADIUS 5
+#define LINE_THICKNESS 3
 
 class Board
 {
@@ -8,9 +10,12 @@ public:
 	Board();
 	void Draw(sf::RenderWindow& window);
 private:
-	std::vector<sf::RectangleShape> m_tiles;
+	std::vector<sf::CircleShape> m_tiles;
 	int m_size;
 	float m_tileSize;
-
+	sf::RectangleShape m_redHorizontalLine1;
+	sf::RectangleShape m_redHorizontalLine2;
+	sf::RectangleShape m_blackVerticalLine1;
+	sf::RectangleShape m_blackVerticalLine2;
 };
 
